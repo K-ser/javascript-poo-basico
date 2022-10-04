2f8bfa706c5d21bd5ccf4745a2383ce09089873e
Kevin Garcia <kevingarcore@gmail.com>
2022-10-03 22:42:15 -0500

    Classes aboaut inheritance are over

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function stopVideo(id) {
   console.log(`Se ha pausado ${urlSecreta}`);
 }
 
-export class listClass {
+class listClass {
   constructor({
     name,
     videoID,
@@ -28,9 +28,14 @@ class Courses {
   constructor ({
     name,
     classes = [],
+    isFree = false,
+    lang = 'spanish',
   }) {
     this._name = name;
     this.classes = classes;
+    this.isFree = isFree;
+    this.lang = lang;
+
   }
   
   get name() {
@@ -55,7 +60,9 @@ class Courses {
 
 
 const cursoPrograBasica = new Courses({
-  name: 'Curso de Programación Básica'
+  name: 'Curso de Programación Básica',
+  isFree: true,
+  lang: 'spanish',
 })
 //tratar de cambiar el nombre del curso
 // cursoPrograBasica.name = 'Nuevo nombre que se le quiera poner'
@@ -64,7 +71,8 @@ const cursoDefinitivoHTML = new Courses({
   name: 'Curso definitivo de HTML y CSS'
 })
 const cursoPracticoHTML = new Courses({
-  name: 'Curso práctico de HTML y CSS'
+  name: 'Curso práctico de HTML y CSS',
+  lang: 'english',
 })
 const cursoDataBusiness = new Courses({
   name: 'Curso DataBusiness'
@@ -140,7 +148,46 @@ class Student {
   }
 }
 
-const kevin = new Student({
+class FreeStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  aproveCourse(newCourse) {
+    if(newCourse.isFree) {
+      this.aprovedCourses.push(newCourse);
+    } else {
+      console.warn(`Lo sentimos ${this.name} solo puedes tomar cursos abiertos`)
+    }
+  }
+}
+
+class BasicStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  aproveCourse(newCourse) {
+    if(newCourse.lang !== 'english') {
+      this.aprovedCourses.push(newCourse);
+    } else {
+      console.warn(`Lo sentimos ${this.name} solo puedes tomar en idioma español`)
+    }
+  }
+}
+
+class ExpertStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  aproveCourse(newCourse) {
+      this.aprovedCourses.push(newCourse);
+  }
+}
+
+
+const kevin = new BasicStudent({
   name: 'Kevin',
   email: 'kevin@gmail.com',
   userName:'K-ser',
@@ -150,3 +197,14 @@ const kevin = new Student({
     escuelaData,
   ],
 });
+
+const Pau = new FreeStudent({
+  name: 'Pua',
+  email: 'pau@gmail.com',
+  userName:'LaPau',
+  facebook: 'Pau Gar',
+  learningPaths: [
+    escuelaWeb,
+    escuelaVgs,
+  ],
+})
